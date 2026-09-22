1c6b877180a1e6abb11186ca19e1e71042af45cf
Mohammed Hassan <medo.hassan0100@gmail.com>
2026-09-22 20:58:10 +0300

    Initial commit

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 |
 */
 
-pest()->extend(Tests\TestCase::class)
+pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
     ->in('Feature');
 
